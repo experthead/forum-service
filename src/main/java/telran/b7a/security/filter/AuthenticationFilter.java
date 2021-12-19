36774b40e6509ecca3e4961c49008cb6b0912e3d
meg2019 <megazoid@protonmail.com>
2021-12-19 16:07:14 +0200

    Added AuthenticationFilter

---
+++ src/main/java/telran/b7a/security/filter/AuthenticationFilter.java
@@ -0,0 +1,30 @@
+package telran.b7a.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		System.out.println(request.getMethod());
+		System.out.println(request.getServletPath());
+		System.out.println(request.getHeader("Authorization"));
+		chain.doFilter(request, response);
+
+	}
+
+}
